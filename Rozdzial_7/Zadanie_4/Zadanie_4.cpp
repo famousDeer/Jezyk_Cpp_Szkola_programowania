760388f9cd9fd741fac36a0581c45d2e4d1935f6
famousDeer <49595988+famousDeer@users.noreply.github.com>
2020-10-22 16:34:36 +0200

    Update Zadanie_4.cpp

--- Rozdzial_7/Zadanie_4/Zadanie_4.cpp
+++ Rozdzial_7/Zadanie_4/Zadanie_4.cpp
@@ -1,1 +1,28 @@
+#include<iostream>
 
+long double probability(unsigned numbers, unsigned picks);
+
+int main(){
+    double total, choices, total_powerball, num_powerball;
+    std::cout << "Podaj najwieksza liczbe, jaka mozna wybrac, oraz\n liczbe skreslen\n";
+    std::cout << "Nastepnie podaj najwieksza liczbe jaka mozna wybrac dla powerball, oraz\n liczbe skreslen\n";
+    while((std::cin >> total >> choices >> total_powerball >> num_powerball) && choices <= total && num_powerball <= total_powerball){
+        std::cout <<"Szansa wygranej to jeden do ";
+        std::cout << probability(total, choices) * probability(total_powerball, num_powerball);
+        std::cout << "\n";
+        std::cout << "Nastepne dwie liczby (q, aby zakonczyc): ";
+    }
+    std::cout << "do widzenia\n";
+    return 0;
+}
+
+long double probability(unsigned numbers, unsigned picks){
+    long double result = 1.0;
+    long double n;
+    unsigned p;
+
+    for(n = numbers, p = picks; p > 0; n--, p--){
+        result = result * n/p;
+        return result;
+    }
+}
